d36793c259536d5b1f5b9415cb45e0d8998f4fea
HP <HP@DESKTOP-BN396F1>
2020-10-10 02:31:47 +0400

    1st commit

---
+++ helloworld_Suraj/src/test/java/helloworld_Suraj/TestHelloWorld.java
@@ -0,0 +1,13 @@
+package helloworld_Suraj;
+
+import org.testng.annotations.Test;
+
+public class TestHelloWorld {
+	
+	@Test
+	public void testHelloWorld(){
+		
+		System.out.println("Hello World");
+	}
+
+}
